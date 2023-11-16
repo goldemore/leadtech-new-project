7a738c758d7ac09e4814bdea33420be44e967828
goldemore <goldemorone@gmail.com>
2023-11-16 22:03:18 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CategoryPage from "./pages/CategoryPage";
 import SingleProdPage from "./pages/SingleProdPage";
@@ -6,14 +6,14 @@ import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category" element={<CategoryPage />} />
         <Route path="/product" element={<SingleProdPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
